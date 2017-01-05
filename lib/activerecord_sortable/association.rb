0b3bf61b537db5c7daf2bd625cbc59962521caa8
vs <vshaveyko@gmail.com>
2017-01-05 21:54:17 +0200

    Do not overwrite existing ActiveRecord monkeypatches on valid_options

--- lib/activerecord_sortable/association.rb
+++ lib/activerecord_sortable/association.rb
@@ -1,15 +1,22 @@
 # encoding: utf-8
+# frozen_string_literal: true
 module ActiveRecord
   module Associations
     module Builder
       class Association #:nodoc:
-        SORTABLE_OPTIONS = [:sortable, :sortable_field].freeze
 
-        def self.valid_options(_)
-          VALID_OPTIONS +
-            SORTABLE_OPTIONS +
-            Association.extensions.flat_map(&:valid_options)
+        class << self
+
+          SORTABLE_OPTIONS = [:sortable, :sortable_field].freeze
+
+          alias original_valid_options valid_options
+
+          def valid_options(*args)
+            original_valid_options(*args) + SORTABLE_OPTIONS
+          end
+
         end
+
       end
     end
   end
